f39b42029bb3ca8df9d0907d49dbf216d1033c7d
Martin Lai <liming.martin.lai@gmail.com>
2020-03-07 11:59:32 -0500

    update styles; add scss file

--- projects/evaluate-news-nlp/src/client/index.js
+++ projects/evaluate-news-nlp/src/client/index.js
@@ -1,3 +1,3 @@
 import "normalize.css"
 import "./styles/main.scss"
-
+import "./styles/form.scss"
--- projects/evaluate-news-nlp/webpack.common.js
+++ projects/evaluate-news-nlp/webpack.common.js
@@ -7,6 +7,10 @@ const commonRules = [
         loader: "babel-loader"
     },
     {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+    },
+    {
         test: /\.scss$/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
     }
